3edb18ef3a9e9862c6880dd916b771045f9c863f
Meetpatel94 <rspatel121073@gail.com>
2024-06-27 13:39:47 +0530

    add some files

---
+++ Python/1_pattern.py
@@ -0,0 +1,29 @@
+# ---------------------------Task-1--------------------------#
+for i in range(1,6):                # *
+     for j in range(1,i+1):         # **
+          print("*",end=" ")        # ***
+     print("")                      # ****
+                                    # *****
+# ---------------------------Task-2--------------------------#
+#
+# for i in range(1,6):               #        * 
+#      for j in range(1,6-i):        #       * *
+#           print(" ",end="")        #    * * * * *
+#      for k in range(1,2*i):        #   * * * * * *
+#         print("*",end="")          #  * * * * * * *
+#      print()                    
+# ---------------------------Task-3--------------------------#
+#
+# for i in range(1,6):                #          *
+#      for j in range(1,6-i):         #         * *
+#           print(" ",end=" ")        #      * * * * *
+#      for k in range(1,2*i):         #     * * * * * *
+#         print("*",end=" ")          #    * * * * * * *
+#      print()                        #    * * * * * * *
+# for i in range(6-1,0,-1):           #     * * * * * *
+#     for j in range(1,6-i):          #      * * * * *
+#           print(" ",end=" ")        #         * *
+#     for k in range(1,2*i):          #          *
+#         print("*",end=" ")
+#     print() 
+
